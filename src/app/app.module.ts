76a1ebd13d41e0b7c329093c35d33442df774244
Ricardo Alfonso Hernandez Sanchez <hernandez.ricardo@neoris.cxnetworks.net>
2018-08-03 09:54:40 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { TweetListComponent } from './components/tweet-list/tweet-list.component';
+import { SuggestionsComponent } from './components/suggestions/suggestions.component';
+import { TweetComponent } from './components/tweet/tweet.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    ProfileComponent,
+    TweetListComponent,
+    SuggestionsComponent,
+    TweetComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
