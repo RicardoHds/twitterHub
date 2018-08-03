becb6b10d10ea091eb1c87052a130aca69c82d69
Salvador Almaraz Montemayor <salvador.almaraz@neoris.cxnetworks.net>
2018-08-03 15:44:53 -0500

    tweet-list

--- src/app/components/tweet-list/tweet-list.component.ts
+++ src/app/components/tweet-list/tweet-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { TweetsService } from '../../services/tweets.service';
+import * as moment from 'moment';
 @Component({
   selector: 'app-tweet-list',
   templateUrl: './tweet-list.component.html',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TweetListComponent implements OnInit {
 
-  constructor() { }
+  constructor(private tweetsService: TweetsService) { }
 
   ngOnInit() {
+  }
+  getTweets() {
+    return this.tweetsService.getTwits();
+  }
+  tiempo(date: any) {
+    const date2 = moment(new Date(date));
+    const hoy = moment(new Date());
+    if ( hoy.diff(date2, 'h') > 0) {
+      return '·' + hoy.diff(date2, 'h') + ' h';
+    } else {
+      return '·' + hoy.diff(date2, 'm') + ' m';
+    }
   }
 
 }
--- src/app/services/tweets.service.ts
+++ src/app/services/tweets.service.ts
@@ -4,9 +4,22 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class TweetsService {
-  private tweet: any[] = [
-    {name: '', alias: '', dateTime: '', tweet: ''}
-  ];
-
   constructor() { }
+
+  private tweet: any[] = [
+    { name: 'Salvador Almaraz', alias: '@salvador.almaraz', dateTime: '8/3/2018 13:10', tweet: 'Hola' },
+    { name: 'Salvador Almaraz', alias: '@salvador.almaraz', dateTime: '8/3/2018 15:10', tweet: 'Hola2' },
+    { name: 'Salvador Almaraz', alias: '@salvador.almaraz', dateTime: '8/3/2018 13:10', tweet: 'hola3' },
+    { name: 'Salvador Almaraz', alias: '@salvador.almaraz', dateTime: '8/3/2018 13:10', tweet: 'Tanta prisa tenemos por hacer, escribir y dejar oír nuestra voz en el silencio de la eternidad, que olvidamos lo único realmente importante: ¡VIVIR!. Robert Louis Stevenson' }
+  ];
+  
+  public getTwits() {
+    return this.tweet.reverse();
+  }
+
+  public postTwits(tweet: string) {
+    const date = new Date();
+    const tweetObj = { name: 'Salvador Almaraz', alias: 'salvador.almaraz', dateTime: date, tweet: tweet};
+    this.tweet.push(tweetObj);
+  }
 }
